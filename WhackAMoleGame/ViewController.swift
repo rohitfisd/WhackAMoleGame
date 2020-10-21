5b98e24baae39342682a90ffb7884c5d2b91281e
Rohit Ramarathinam <rohit.ramarathinam.017@k12.friscoisd.org>
2020-10-21 14:18:25 -0500

    Initial Commit

---
+++ WhackAMoleGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhackAMoleGame
+//
+//  Created by Ramarathinam, Rohit on 10/21/20.
+//  Copyright © 2020 Ramarathinam, Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
